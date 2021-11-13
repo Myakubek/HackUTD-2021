f8366d3086d0f2babd7df7826df5341c192c4e51
CristiKo <62083686+CristiKo@users.noreply.github.com>
2021-11-13 17:10:13 -0600

    Update SortStore.py

--- State Farm Classifier/SortStore.py
+++ State Farm Classifier/SortStore.py
@@ -1,35 +1,35 @@
 #Class to sort the reviews by keyword
-if (review.policy = "Vehicle"):
-  if (review.policyType = "Car"):
-  if (review.policyType = "Motorcycle"):
-  if (review.policyType = "Boat"):
-  if (review.policyType = "Motorhome"):
-  if (review.policyType = "Off-road"):
+if (review.policy == "Vehicle"):
+  if (review.policyType == "Car"):
+  if (review.policyType == "Motorcycle"):
+  if (review.policyType == "Boat"):
+  if (review.policyType == "Motorhome"):
+  if (review.policyType == "Off-road"):
     
-if (review.policy = "Home"):
-  if (review.policyType = "Homeowner"):
-  if (review.policyType = "Condo"):
-  if (review.policyType = "Rent"):
+if (review.policy == "Home"):
+  if (review.policyType == "Homeowner"):
+  if (review.policyType == "Condo"):
+  if (review.policyType == "Rent"):
     
-if (review.policy = "Life"):
-  if (review.policyType = "Whole"):
-  if (review.policyType = "Universal"):
-  if (review.policyType = "Variable"):
+if (review.policy == "Life"):
+  if (review.policyType == "Whole"):
+  if (review.policyType == "Universal"):
+  if (review.policyType == "Variable"):
     
-if (review.policy = "Health"):
-  if (review.policyType = "Health"):
-  if (review.policyType = "Disability"):
-  if (review.policyType = "Liability"):
+if (review.policy == "Health"):
+  if (review.policyType == "Health"):
+  if (review.policyType == "Disability"):
+  if (review.policyType == "Liability"):
     
 if (review.policy = "Small Business"):
-  if (review.policyType = "General liability insurance"):
-  if (review.policyType = "Professional liability insurance"):
-  if (review.policyType = "Business income coverage"):
-  if (review.policyType = "Commercial property insurance"):
-  if (review.policyType = "Workers’ compensation insurance"):
-  if (review.policyType = "Commercial auto insurance"):
-  if (review.policyType = "Commercial umbrella insurance"):
-  if (review.policyType = "Business income coverage"):
-  if (review.policyType = "Employment Practices Liability Insurance"):
-  if (review.policyType = "Business Owner’s Policy (BOP)"):
+  if (review.policyType == "General liability insurance"):
+  if (review.policyType == "Professional liability insurance"):
+  if (review.policyType == "Business income coverage"):
+  if (review.policyType == "Commercial property insurance"):
+  if (review.policyType == "Workers’ compensation insurance"):
+  if (review.policyType == "Commercial auto insurance"):
+  if (review.policyType == "Commercial umbrella insurance"):
+  if (review.policyType == "Business income coverage"):
+  if (review.policyType == "Employment Practices Liability Insurance"):
+  if (review.policyType == "Business Owner’s Policy (BOP)"):
     
