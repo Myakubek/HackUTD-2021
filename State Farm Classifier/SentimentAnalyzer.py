633c3ebdd76f2e374826a13fcd906f4a95643acd
Matthew Yakubek <myakubek@yahoo.com>
2021-11-13 17:06:19 -0600

    update sentimentAnalyzer

--- State Farm Classifier/SentimentAnalyzer.py
+++ State Farm Classifier/SentimentAnalyzer.py
@@ -1,5 +1,6 @@
 #imports
-import nltk, nltk.data
+import nltk
+import nltk.data
 from nltk.sentiment import SentimentIntensityAnalyzer
 import matplotlib.pyplot as plt
 nltk.download('punkt')
@@ -8,7 +9,12 @@ nltk.download('stopwords')
 
 class sentimentAnalyzer:
     #Function to convert a list to string for stopword formatting/conversion
-    def listToString(string):
+    def __init__(self, text):
+        self.text = text
+        self.negative = self.getNegative(text)
+        self.positive = self.getPositive(text)
+
+    def listToString(self, string):
         str = ""
         #Loop and add each element of list
         for s in string:
@@ -18,16 +24,24 @@ class sentimentAnalyzer:
 
 
     #Gets a string, splits into a list and filters out stopwards, returns formatted string
-    def removeStopwords(Text):
+    def removeStopwords(self, Text):
         Text = Text.split(" ")                                  #Split text into seperated list
         stopwords = nltk.corpus.stopwords.words("english")           #import a list of stopwords from nltk
         Text = [w for w in Text if w.lower() not in stopwords]       #Remove the stopwords from Text (becomes list of words)
-        return Text.listToString(Text)
+        return self.listToString(Text)
 
 
     #Use nltk's pre-trained sentiment analyzer (VADER) to analyze a given text
-    def analyzeSentiment(Input):
+    def analyze(self, input):
         sia = SentimentIntensityAnalyzer()    #Instantiate sentiment analyzer object
-        scores = sia.polarity_scores(Input)   #Returns a dictionary of sentiment scores
+        input = self.removeStopwords(input)
+        scores = sia.polarity_scores(input)   #Returns a dictionary of sentiment scores
         return scores
 
+
+    def getNegative(self, text):
+        return (format(self.analyze(text).get('neg')*100, '.1f') + '%')
+
+
+    def getPositive(self, text):
+        return (format(self.analyze(text).get('pos')*100, '.1f') + '%')
