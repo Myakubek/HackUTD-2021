db6014fb55f05005a98ca04e01e663172f3cb95b
Matthew Yakubek <myakubek@yahoo.com>
2021-11-13 17:16:03 -0600

    update & link to sentiment analyzer

--- State Farm Classifier/ReviewInstance.py
+++ State Farm Classifier/ReviewInstance.py
@@ -1,9 +1,13 @@
+#Sentiment analyzer testbench
+import SentimentAnalyzer as sa
+
 #Class to create an instance of reviews
 class Review:
-  review
-  reviewerName
-  date
-  flo sentimentVal
-  flo memberLength
-  policy
-  policyType
+  def __init__(self, review, name, date, memberLength, policy, policyType):
+    self.review = review
+    self.name = name
+    self.date = date
+    self.sentimentVal = sa.sentimentAnalyzer(review)
+    self.memberLength = memberLength
+    self.policy = policy
+    self.policyType = policyType
