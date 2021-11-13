6d11c753e509bb71b7d5774c5a8946f85673ecd6
Matthew Yakubek <myakubek@yahoo.com>
2021-11-13 16:48:30 -0600

    hook testbench & setup as class

--- State Farm Classifier/SentimentAnalyzer.py
+++ State Farm Classifier/SentimentAnalyzer.py
@@ -19,7 +19,7 @@ class sentimentAnalyzer:
 
     #Gets a string, splits into a list and filters out stopwards, returns formatted string
     def removeStopwords(Text):
-        Text = textInput.split(" ")                                  #Split text into seperated list
+        Text = Text.split(" ")                                  #Split text into seperated list
         stopwords = nltk.corpus.stopwords.words("english")           #import a list of stopwords from nltk
         Text = [w for w in Text if w.lower() not in stopwords]       #Remove the stopwords from Text (becomes list of words)
         return Text.listToString(Text)
@@ -31,45 +31,3 @@ class sentimentAnalyzer:
         scores = sia.polarity_scores(Input)   #Returns a dictionary of sentiment scores
         return scores
 
-#Gets a dictionary of scores from sentiment intensity analyzer and creates a pie chart of pos, neg, and neutral values
-def pieChart(scores):
-    labels = 'Negative', 'Neutral', 'Positive'
-    sizes = [scores.get('neg'), scores.get('neu'), scores.get('pos')]
-    fig1, ax1 = plt.subplots()
-    ax1.pie(sizes, labels = labels, autopct = '%1.1f%%', shadow = True, startangle=90, normalize=True)
-    ax1.axis('equal')
-    plt.show()
-
-
-#format the scores from the sentiment analyzer (given as dictioanry) to print the percentages of results
-def printScore(scores):
-    print('Negative:', format(scores.get('neg')*100, '.1f'), end = '% ')
-    print('Neutral:', format(scores.get('neu')*100, '.1f'), end = '% ')
-    print('Positive:', format(scores.get('pos')*100, '.1f'), end = '% ')
-    print('Compound:', format(scores.get('compound'), '.1f'))
-
-
-#Read in the file as a String, remove newlines and quotation marks
-with open('input.txt', 'r') as file:
-    tokenizer = nltk.data.load('tokenizers/punkt/english.pickle')    #algorithm to find sentence boundaries
-    textInput = file.read().replace('\n', ' ').replace('\"', '')     #Get raw text, remove quotations and newlines
-    sentenceInput = tokenizer.tokenize(textInput)                    #detect and split sentences/
-
-
-#Print and graph the resulting scores for raw text & text with stopwords removed
-print("Overall Sentiment Score of raw text")
-myScore = analyzeSentiment(textInput)
-printScore(myScore)
-pieChart(myScore)
-
-print("Overall Sentiment Score of text cleaned of stopwords")
-myScore = analyzeSentiment(removeStopwords(textInput))
-printScore(myScore)
-pieChart(myScore)
-
-#Iteratively display sentiment scores for each sentence
-print("-----Sentiment by Sentence-----\n")
-for i in sentenceInput:
-    print("Sentiment of sentence: ", i)
-    printScore(analyzeSentiment(i))
-    print()
--- State Farm Classifier/testBench.py
+++ State Farm Classifier/testBench.py
@@ -1,2 +1,8 @@
+#Sentiment analyzer testbench
 import SentimentAnalyzer
+
+review = 'I have been with State Farm for several years now and see no reason to change. Local agent is great and the service level is unmatched by any of the others that I have used over the years. I routinely check prices and other quotes and the minimal difference is not worth the hassle of changing companies. Claims are handled well and quickly.'
+analyzer = SentimentAnalyzer.sentimentAnalyzer.analyzeSentiment(review)
+
+print(analyzer)
 
