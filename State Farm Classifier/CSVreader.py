81bd07b974ca4904d91c0ee4f048afc87c411bb0
Matthew Yakubek <myakubek@yahoo.com>
2021-11-13 20:51:04 -0600

    update CSV reader

--- State Farm Classifier/CSVreader.py
+++ State Farm Classifier/CSVreader.py
@@ -7,41 +7,5 @@ def readCSV(fileName):
     reviews = []
 
     for i in df:
-<<<<<<< HEAD
         reviews.append(Review(i))
     return reviews
-=======
-        print(i)
-    return df
-
-l = reviewlist.size()
-
-vehicleList = [l][]
-homeList = [l][]
-lifeList = [l][]
-healthList = [l][]
-smallBusinessList = [l][]
-policyList = [vehicleList, homeList, lifeList, healthList, smallBusinessList]
-
-    for (x = 0; x < l; x++):
-        post.review = reviewList[x][0]
-        post.reviewerName = reviewList[x][1]
-        post.date = reviewList[x][2]
-        post.rmemberLength = reviewList[x][3]
-        post.policy = reviewList[x][4]
-        post.policyType = reviewList[x][5]
-        
-        if (post.policy == "Vehicle"):
-            vehicleList.append(post)
-        if (post.policy == "Home"):
-            homeList.append(post)
-        if (post.policy == "Life"):
-            lifeList.append(post)
-        if (post.policy == "Health"):
-            healthList.append(post)
-        if (post.policy = "Small Business"):
-            smallBusinessList.append(post)
-        
-        
-            
->>>>>>> 888cd10d1514e897552a4d0911fbd976e1743b60
